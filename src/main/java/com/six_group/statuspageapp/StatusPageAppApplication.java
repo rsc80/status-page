96da327908a7eaf0d8553ce2bf65935a40065150
Schlegel, Roman <roman.schlegel@six-group.com>
2024-09-12 22:37:55 +0200

    added spring-boot app

---
+++ src/main/java/com/six_group/statuspageapp/StatusPageAppApplication.java
@@ -0,0 +1,13 @@
+package com.six_group.statuspageapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusPageAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StatusPageAppApplication.class, args);
+  }
+
+}
