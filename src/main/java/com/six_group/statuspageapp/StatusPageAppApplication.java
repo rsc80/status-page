08204bce1ac332ca2662ba9389a73301f93e4275
Gotzen, Fabian <fabian.gotzen@six-group.com>
2024-09-13 16:39:53 +0200

    add redis and basic controller / service / repository skeleton

--- src/main/java/com/six_group/statuspageapp/StatusPageAppApplication.java
+++ src/main/java/com/six_group/statuspageapp/StatusPageAppApplication.java
@@ -4,7 +4,6 @@ import com.redis.om.spring.annotations.EnableRedisDocumentRepositories;
 import com.six_group.statuspageapp.api.dto.StatusIndicator;
 import com.six_group.statuspageapp.domain.Application;
 import com.six_group.statuspageapp.persistence.ApplicationRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,19 +15,67 @@ import java.time.LocalDate;
 @EnableRedisDocumentRepositories(basePackages = "com.six_group.statuspageapp.domain.*")
 public class StatusPageAppApplication {
 
-  @Autowired
-  ApplicationRepository applicationRepository;
+    final
+    ApplicationRepository applicationRepository;
 
-  @Bean
-  CommandLineRunner loadTestData() {
-    return args -> {
-      applicationRepository.deleteAll();
-      Application cs = Application.newInstance("Redis", "https://redis.com", LocalDate.of("2024-09-01"), 23, StatusIndicator.SUCCESS, );
-      applicationRepository.save(cs);
-    };
-  }
-  public static void main(String[] args) {
-    SpringApplication.run(StatusPageAppApplication.class, args);
+    public StatusPageAppApplication(ApplicationRepository applicationRepository) {
+        this.applicationRepository = applicationRepository;
+    }
 
-  }
+    @Bean
+    CommandLineRunner loadTestData() {
+        return _ -> {
+            applicationRepository.deleteAll();
+            Result result = getApplications();
+            applicationRepository.save(result.ZKB());
+            applicationRepository.save(result.CS());
+            applicationRepository.save(result.UBS());
+        };
+    }
+
+    private static Result getApplications() {
+        Application ZKB = Application.newInstance(
+                "1",
+                "ZKB",
+                LocalDate.parse("2024-09-01"),
+                13,
+                StatusIndicator.SUCCESS,
+                "200",
+                1000,
+                2,
+                1
+        );
+        Application CS = Application.newInstance(
+                "2",
+                "CS",
+                LocalDate.parse("2024-09-01"),
+                13,
+                StatusIndicator.WARNING,
+                "400",
+                1000,
+                2,
+                1
+        );
+        Application UBS = Application.newInstance(
+                "3",
+                "UBS",
+                LocalDate.parse("2024-09-01"),
+                13,
+                StatusIndicator.DANGER,
+                "600",
+                1000,
+                500,
+                500
+        );
+        Result result = new Result(ZKB, CS, UBS);
+        return result;
+    }
+
+    private record Result(Application ZKB, Application CS, Application UBS) {
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatusPageAppApplication.class, args);
+
+    }
 }
