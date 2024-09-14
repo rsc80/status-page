f56dec4c61669007df049af524a740c7d7dd9dad
Graf, Thomas <thomas.graf@six-group.com>
2024-09-14 11:41:33 +0200

    add post hourly

--- src/main/java/com/six_group/statuspageapp/api/ParticipantController.java
+++ src/main/java/com/six_group/statuspageapp/api/ParticipantController.java
@@ -8,6 +8,7 @@ import com.six_group.statuspageapp.domain.ParticipantDailyOverviewDto;
 import com.six_group.statuspageapp.domain.ParticipantService;
 import com.six_group.statuspageapp.domain.participant.DayData;
 import com.six_group.statuspageapp.domain.participant.HourlyMetrics;
+import com.six_group.statuspageapp.domain.participant.Service;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -20,37 +21,54 @@ import java.util.List;
 @CrossOrigin("http://localhost:4200")
 public class ParticipantController {
 
-  private final ParticipantService participantService;
+    private final ParticipantService participantService;
 
-  public ParticipantController(ParticipantService participantService) {
-    this.participantService = participantService;
-  }
+    public ParticipantController(ParticipantService participantService) {
+        this.participantService = participantService;
+    }
 
-  @GetMapping("/participants")
-  public List<ParticipantDto> getAllParticipants() throws IOException {
-    return this.participantService.getAllParticipants();
-  }
+    @GetMapping("/participants")
+    public List<ParticipantDto> getAllParticipants() throws IOException {
+        return this.participantService.getAllParticipants();
+    }
 
-  @GetMapping("/participants-overview")
-  public List<ParticipantOverviewDto> getParticipantsOverview() throws IOException {
-    return this.participantService.getParticipantOverview();
-  }
+    @GetMapping("/participants-overview")
+    public List<ParticipantOverviewDto> getParticipantsOverview() throws IOException {
+        return this.participantService.getParticipantOverview();
+    }
 
-  @GetMapping("/participants/{id}/{day}")
-  public ParticipantDailyOverviewDto getParticipantById(@PathVariable String id, @PathVariable String day) {
-    return this.participantService.getParticipantByIdAndDay(id, day);
-  }
+    @GetMapping("/participants/{id}/{day}")
+    public ParticipantDailyOverviewDto getParticipantById(@PathVariable String id, @PathVariable String day) {
+        return this.participantService.getParticipantByIdAndDay(id, day);
+    }
 
-  @PostMapping("/participants/{id}/{serviceName}/{serviceVersion}")
-  public void postParticipantById(@PathVariable String id, @PathVariable String serviceName, @PathVariable String serviceVersion, @RequestBody HourlyMetrics  hourlyMetrics) {
-    var participant = StatusPageAppApplication.PARTICIPANT_MAP.get(id);
-    var date = LocalDate.now().toString();
-    var hour = "%02d:00".formatted(LocalDateTime.now().getHour());
-    participant.getDailyData().stream().filter(v -> v.getDate().equals(date))
-            .map(DayData::getServices)
-            .flatMap(List::stream)
-            .filter(v -> v.getServiceName().equalsIgnoreCase(serviceName) && v.getServiceVersion().equalsIgnoreCase(serviceVersion))
-            .findFirst()
-            .ifPresent(v -> v.getHours().put(hour, hourlyMetrics));
-  }
+    @PostMapping("/participants/{id}/{serviceName}/{serviceVersion}")
+    public void postParticipantById(@PathVariable String id, @PathVariable String serviceName, @PathVariable String serviceVersion, @RequestBody HourlyMetrics hourlyMetrics) {
+        var participant = StatusPageAppApplication.PARTICIPANT_MAP.get(id);
+        if (participant != null) {
+            var date = LocalDate.now().toString();
+            var hour = "%02d:00".formatted(LocalDateTime.now().getHour());
+
+            var dailyData = participant.getDailyData().stream().filter(v -> v.getDate().equals(date)).findFirst().orElseGet(() -> {
+                var dayData = new DayData();
+                dayData.setDate(date);
+                participant.getDailyData().add(dayData);
+                return dayData;
+            });
+
+            var service = dailyData.getServices().stream()
+                    .filter(v -> v.getServiceName().equalsIgnoreCase(serviceName) && v.getServiceVersion().equalsIgnoreCase(serviceVersion))
+                    .findFirst()
+                    .orElseGet(() -> {
+                        var serviceData = new Service();
+                        serviceData.setServiceName(serviceName);
+                        serviceData.setServiceVersion(serviceVersion);
+                        serviceData.setHours(new java.util.HashMap<>());
+                        dailyData.getServices().add(serviceData);
+                        return serviceData;
+                    });
+
+            service.getHours().put(hour, hourlyMetrics);
+        }
+    }
 }
