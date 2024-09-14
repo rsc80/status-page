c8f96bcbbaff30b26cc21169144f0e71ab6a89b7
Gotzen, Fabian <fabian.gotzen@six-group.com>
2024-09-14 08:10:45 +0200

    update data model and load test data from json

--- src/main/java/com/six_group/statuspageapp/domain/ParticipantService.java
+++ src/main/java/com/six_group/statuspageapp/domain/ParticipantService.java
@@ -2,10 +2,8 @@ package com.six_group.statuspageapp.domain;
 
 
 import com.six_group.statuspageapp.api.dto.ParticipantDto;
-import com.six_group.statuspageapp.domain.participant.DailyMetrics;
-import com.six_group.statuspageapp.domain.participant.DayData;
-import com.six_group.statuspageapp.domain.participant.HourlyMetrics;
-import com.six_group.statuspageapp.domain.participant.Participant;
+import com.six_group.statuspageapp.api.dto.StatusIndicator;
+import com.six_group.statuspageapp.domain.participant.*;
 import com.six_group.statuspageapp.persistence.ParticipantRepository;
 import org.springframework.stereotype.Service;
 
@@ -16,17 +14,20 @@ public class ParticipantService {
 
     private final ParticipantRepository participantRepository;
 
+    private static final double SUCCESS_THRESHOLD = 0.05;
+    private static final double WARNING_THRESHOLD = 0.10;
+
     public ParticipantService(ParticipantRepository participantRepository) {
         this.participantRepository = participantRepository;
     }
 
     public List<ParticipantDto> getAllParticipants() {
-         var participants = participantRepository.findAll();
+        var participants = participantRepository.findAll();
 
-         return participants.parallelStream()
-                 .map(this::aggregateDailyMetricsForParticipant)
-                 .map(participant -> participant.toDto(participant))
-                 .toList();
+        return participants.parallelStream()
+                .map(this::aggregateDailyMetricsForParticipant)
+                .map(participant -> participant.toDto(participant))
+                .toList();
     }
 
     public ParticipantDto getParticipantById(String id) {
@@ -44,26 +45,45 @@ public class ParticipantService {
 
         return participant;
     }
-    private DayData aggregateDailyMetrics(DayData dayData){
+
+    private DayData aggregateDailyMetrics(DayData dayData) {
         int totalRequests = 0;
         int totalSuccessCount = 0;
         int totalClientErrorCount = 0;
         int totalServerErrorCount = 0;
 
-        for(HourlyMetrics hourlyData : dayData.getHours().values()){
-            totalSuccessCount += hourlyData.getSuccessCount();
-            totalClientErrorCount += hourlyData.getClientErrorCount();
-            totalServerErrorCount += hourlyData.getServerErrorCount();
-            totalRequests = totalClientErrorCount + totalServerErrorCount + totalSuccessCount;
+        for (Api api : dayData.getApis()) {
+            for (HourlyMetrics hourlyMetrics : api.getHours().values()){
+            totalSuccessCount += hourlyMetrics.getSuccessCount();
+            totalClientErrorCount += hourlyMetrics.getClientErrorCount();
+            totalServerErrorCount += hourlyMetrics.getServerErrorCount();
+            totalRequests = totalSuccessCount + totalClientErrorCount + totalServerErrorCount;
+            }
+            buildDailyMetrics(api, totalRequests, totalSuccessCount, totalClientErrorCount, totalServerErrorCount);
+            calculateStatusIndicator(dayData, totalServerErrorCount, totalRequests);
         }
+        return dayData;
+    }
 
+    private static void buildDailyMetrics(Api api, int totalRequests, int totalSuccessCount, int totalClientErrorCount, int totalServerErrorCount) {
         DailyMetrics dailyMetrics = new DailyMetrics();
         dailyMetrics.setTotalRequests(totalRequests);
         dailyMetrics.setTotalSuccessCount(totalSuccessCount);
         dailyMetrics.setTotalClientErrorCount(totalClientErrorCount);
         dailyMetrics.setTotalServerErrorCount(totalServerErrorCount);
-        dayData.setDailyMetrics(dailyMetrics);
-        return dayData;
+        api.setDailyMetrics(dailyMetrics);
+    }
+
+    private static void calculateStatusIndicator(DayData dayData, double totalServerErrorCount, int totalRequests) {
+        double serverErrorPercentage = totalServerErrorCount / totalRequests;
+
+        if (serverErrorPercentage <= SUCCESS_THRESHOLD) {
+            dayData.setStatusIndicator(StatusIndicator.SUCCESS);
+        } else if (serverErrorPercentage <= WARNING_THRESHOLD) {
+            dayData.setStatusIndicator(StatusIndicator.WARNING);
+        } else {
+            dayData.setStatusIndicator(StatusIndicator.DANGER);
+        }
     }
 
 }
--- src/main/java/com/six_group/statuspageapp/domain/participant/DayData.java
+++ src/main/java/com/six_group/statuspageapp/domain/participant/DayData.java
@@ -2,7 +2,8 @@ package com.six_group.statuspageapp.domain.participant;
 
 import com.six_group.statuspageapp.api.dto.StatusIndicator;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DayData {
 
@@ -10,35 +11,21 @@ public class DayData {
 
     private StatusIndicator statusIndicator;
 
-
-    private DailyMetrics dailyMetrics;
-
-    private Map<String, HourlyMetrics> hours;
+    private List<Api> apis = new ArrayList<Api>();
 
     public DayData() {
+
     }
 
-    public DayData(String date, StatusIndicator statusIndicator, DailyMetrics dailyMetrics, Map<String, HourlyMetrics> hours) {
+
+    public DayData(String date, StatusIndicator statusIndicator, List<Api> apis) {
         this.date = date;
         this.statusIndicator = statusIndicator;
-        this.dailyMetrics = dailyMetrics;
-        this.hours = hours;
+        this.apis = apis;
     }
 
     public String getDate() {
         return date;
-    }
-
-    public StatusIndicator getStatusIndicator() {
-        return statusIndicator;
-    }
-
-    public DailyMetrics getDailyMetrics() {
-        return dailyMetrics;
-    }
-
-    public Map<String, HourlyMetrics> getHours() {
-        return hours;
     }
 
     public DayData setDate(String date) {
@@ -46,18 +33,21 @@ public class DayData {
         return this;
     }
 
+    public StatusIndicator getStatusIndicator() {
+        return statusIndicator;
+    }
+
     public DayData setStatusIndicator(StatusIndicator statusIndicator) {
         this.statusIndicator = statusIndicator;
         return this;
     }
 
-    public DayData setDailyMetrics(DailyMetrics dailyMetrics) {
-        this.dailyMetrics = dailyMetrics;
-        return this;
+    public List<Api> getApis() {
+        return apis;
     }
 
-    public DayData setHours(Map<String, HourlyMetrics> hours) {
-        this.hours = hours;
+    public DayData setApis(List<Api> apis) {
+        this.apis = apis;
         return this;
     }
 }
