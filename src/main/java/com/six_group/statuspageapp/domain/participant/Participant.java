9cae5eceb1eb4a53d9f9899050ef051f1a8cb009
Gotzen, Fabian <fabian.gotzen@six-group.com>
2024-09-14 13:29:11 +0200

    add toplevel statusindicator

--- src/main/java/com/six_group/statuspageapp/domain/participant/Participant.java
+++ src/main/java/com/six_group/statuspageapp/domain/participant/Participant.java
@@ -2,77 +2,81 @@ package com.six_group.statuspageapp.domain.participant;
 
 import com.six_group.statuspageapp.api.dto.ParticipantDto;
 import com.six_group.statuspageapp.api.dto.ParticipantOverviewDto;
+import com.six_group.statuspageapp.api.dto.StatusIndicator;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Participant {
-  private String id;
+    private String id;
 
-  private String name;
+    private String name;
 
-  private Boolean isExternal;
+    private Boolean isExternal;
 
-  private List<DayData> dailyData = new ArrayList<>();
+    private List<DayData> dailyData = new ArrayList<>();
 
-  public Participant() {
-  }
+    public Participant() {
+    }
 
-  public String getId() {
-    return id;
-  }
+    public String getId() {
+        return id;
+    }
 
-  public Participant setId(String id) {
-    this.id = id;
-    return this;
-  }
+    public Participant setId(String id) {
+        this.id = id;
+        return this;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public Participant setName(String name) {
-    this.name = name;
-    return this;
-  }
+    public Participant setName(String name) {
+        this.name = name;
+        return this;
+    }
 
-  public Boolean getIsExternal() {
-    return isExternal;
-  }
+    public Boolean getIsExternal() {
+        return isExternal;
+    }
 
-  public void setExternal(Boolean external) {
-    isExternal = external;
-  }
+    public void setExternal(Boolean external) {
+        isExternal = external;
+    }
 
-  public List<DayData> getDailyData() {
-    return dailyData;
-  }
+    public List<DayData> getDailyData() {
+        return dailyData;
+    }
 
-  public void setDailyData(List<DayData> dailyData) {
-    this.dailyData = dailyData;
-  }
+    public void setDailyData(List<DayData> dailyData) {
+        this.dailyData = dailyData;
+    }
 
-  public ParticipantDto toDto(Participant participant) {
-    return new ParticipantDto(
-        participant.getId(),
-        participant.getName(),
-        participant.getDailyData()
-    );
-  }
+    public ParticipantDto toDto(Participant participant) {
+        return new ParticipantDto(
+                participant.getId(),
+                participant.getName(),
+                participant.getDailyData()
+        );
+    }
 
-  public ParticipantOverviewDto toOverviewDto(Participant participant) {
-    List<DayDataOverview> dayDataOverviewDtos = participant.getDailyData().stream()
-        .map(dayData -> new DayDataOverview(dayData.getDate(),
-            dayData.getStatusIndicator(),
-            dayData.getServices()
-                .stream().map(service ->
-                    new ServiceOverview(service.getServiceName(), service.getServiceVersion(), service.getDailyMetrics())).toList()
-        )).collect(Collectors.toList());
-    return new ParticipantOverviewDto(
-        participant.getId(),
-        participant.getName(),
-        participant.getIsExternal(),
-        dayDataOverviewDtos);
-  }
+    public ParticipantOverviewDto toOverviewDto(Participant participant) {
+        List<DayDataOverview> dayDataOverviewDtos = participant.getDailyData().stream()
+                .map(dayData -> new DayDataOverview(dayData.getDate(),
+                        dayData.getStatusIndicator(),
+                        dayData.getServices()
+                                .stream().map(service ->
+                                        new ServiceOverview(service.getServiceName(), service.getServiceVersion(), service.getDailyMetrics())).toList()
+                )).collect(Collectors.toList());
+
+
+        return new ParticipantOverviewDto(
+                participant.getId(),
+                participant.getName(),
+                participant.getIsExternal(),
+                StatusIndicator.SUCCESS,
+                dayDataOverviewDtos);
+    }
 }
